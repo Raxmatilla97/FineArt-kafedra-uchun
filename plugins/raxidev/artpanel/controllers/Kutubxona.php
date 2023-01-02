946582cd7bca251482a9dac76a5f6c442f9b1af9
Raxmatilla97 <wi.fi.xor@gmail.com>
2023-01-02 21:40:28 +0500

    Yangi pluginni so'ngi o'zgarishlari

---
+++ plugins/raxidev/artpanel/controllers/Kutubxona.php
@@ -0,0 +1,18 @@
+<?php namespace RaxiDev\ArtPanel\Controllers;
+
+use Backend\Classes\Controller;
+use BackendMenu;
+
+class Kutubxona extends Controller
+{
+    public $implement = [        'Backend\Behaviors\ListController',        'Backend\Behaviors\FormController'    ];
+    
+    public $listConfig = 'config_list.yaml';
+    public $formConfig = 'config_form.yaml';
+
+    public function __construct()
+    {
+        parent::__construct();
+        BackendMenu::setContext('RaxiDev.ArtPanel', 'main-menu-item', 'side-menu-item3');
+    }
+}
