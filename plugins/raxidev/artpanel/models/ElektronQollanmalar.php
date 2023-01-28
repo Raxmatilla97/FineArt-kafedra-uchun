bd156a54749abc93f3b3be6da087113cd41a02dd
Raxmatilla97 <wi.fi.xor@gmail.com>
2023-01-29 01:43:30 +0500

    Yangi plagin qismini ishladim

--- plugins/raxidev/artpanel/models/ElektronQollanmalar.php
+++ plugins/raxidev/artpanel/models/ElektronQollanmalar.php
@@ -2,6 +2,8 @@
 
 use Model;
 
+use BackendAuth;
+
 /**
  * Model
  */
@@ -9,6 +11,9 @@ class ElektronQollanmalar extends Model
 {
     use \October\Rain\Database\Traits\Validation;
     
+     // $slugs ishlashi uchun Sluggable import qilinadi
+    use \October\Rain\Database\Traits\Sluggable;
+
 
     /**
      * @var string The database table used by the model.
@@ -20,4 +25,35 @@ class ElektronQollanmalar extends Model
      */
     public $rules = [
     ];
+
+      // Agar slug ga avtomatik i++ son qo'shilishini istasak pastdagi metod yoziladi
+    protected $slugs = ['slug' => 'title'];
+
+     // Agar formada surat yoki fayl yuklash kerak bo'lsa shu metod orqali uni amalga oshirish mumkin
+    public $attachOne = [        
+        'image' => ['System\Models\File'],
+        'file' => ['System\Models\File'],
+        'url_html' => ['System\Models\File'],
+          
+
+    ];
+
+     
+
+
+    // Agar saqlashdan oldin nimadur amaliyot qilish zarur bo'lsa beforeSave() funksiyasidan foydalanish mumkin
+     public function beforeSave(){ 
+        $user = BackendAuth::getUser(); 
+        $this->user_id = $user->id; 
+        // $this->user_id - refers to your table column
+    }
+
+    public function getFullNameAttribute(){
+        return $this->firstname." ".$this->lastname;
+    }
+
+     // Agar modelda biron tablitsiyaga relation ishlatish kerak bo'lsa uni shu metod orqali bajariladi
+
+    public $belongsTo = ['user'=>'Backend\Models\User'];
+
 }
